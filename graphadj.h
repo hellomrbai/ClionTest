89db4b6fdfba38d71c8896a70aea997f7f57e12a
roosevelt <1243066632@qq.com>
2017-05-01 22:02:58 +0800

    test

---
+++ graphadj.h
@@ -0,0 +1,75 @@
+//
+// Created by roosevelt on 2017/4/30.
+//
+
+#ifndef C_GRAPHADJ_H
+#define C_GRAPHADJ_H
+
+#include <cstdlib>
+#include <cstdio>
+
+#define MaxVertexNum 100
+#define INFINITY 65535
+typedef int Vertex;
+typedef int WeightType;
+typedef char DataType;
+
+typedef struct ENode *PtrToENode;
+struct ENode{
+    Vertex V1,V2;
+    WeightType Weight;
+
+};
+typedef PtrToENode Edge;
+typedef struct GNode *PtrToGNode;
+struct GNode{
+    int Nv;
+    int Ne;
+    WeightType G[MaxVertexNum][MaxVertexNum];
+    DataType Data[MaxVertexNum];
+};
+typedef PtrToGNode MGraph;
+MGraph CreateGraph(int VertexNum){
+    Vertex V,W;
+    MGraph Graph;
+
+    Graph = (MGraph)malloc(sizeof(struct GNode));
+    Graph->Nv = VertexNum;
+    Graph->Ne = 0;
+    for (V = 0;V<Graph->Nv;V++)
+    {
+        for(W=0;W<Graph->Nv;W++){
+            Graph->G[V][W] = INFINITY;
+        }
+    }
+    return Graph;
+}void InsertEdge( MGraph Graph, Edge E )
+{
+    /* 插入边 <V1, V2> */
+    Graph->G[E->V1][E->V2] = E->Weight;
+    /* 若是无向图，还要插入边<V2, V1> */
+    Graph->G[E->V2][E->V1] = E->Weight;
+}
+
+MGraph BuildGraph(){
+    MGraph Graph;
+    Edge E;
+    Vertex V;
+    int Nv,i;
+
+    scanf("%d",&Nv);
+    Graph = CreateGraph(Nv);
+
+    scanf("%d",&(Graph->Ne));
+    if(Graph->Ne != 0){
+        E = (Edge)malloc(sizeof(struct ENode));
+        for(i = 0;i<Graph->Ne;i++) {
+            scanf("%d %d %d", &E->V1, &E->V2, &E->Weight);
+            InsertEdge(Graph,E);
+        }
+    }
+    for(V = 0;V<Graph->Nv;V++)
+        scanf(" %c", &(Graph->Data[V]));
+    return Graph;
+}
+#endif //C_GRAPHADJ_H
